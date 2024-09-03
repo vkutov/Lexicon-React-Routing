eb2db2aa1e184bdb72d6b3679674f19e129d180a
vw rabbit <vkutov@gmail.com>
2024-09-03 16:08:09 +0200

    added no page an redirection

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import Contact from "./pages/Contact";
 import Dashboard from "./pages/Dashboard";
 import Stats from "./pages/stats";
 import Settings from "./pages/settings";
+import NoPage from "./pages/no-page";
+import Oldhome from "./pages/old-home";
+
 
 
 // import NoPage from "./pages/NoPage";
@@ -23,9 +26,8 @@ export default function App() {
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="dashboard/stats" element={<Stats />} />
           <Route path="dashboard/settings" element={<Settings />} />
-
-
-          {/* <Route path="*" element={<NoPage />} /> */}
+          <Route path="*" element={<NoPage />} />
+          <Route path="old-home" element={<Oldhome />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/old-home.tsx
@@ -0,0 +1,8 @@
+import { Navigate } from "react-router-dom";
+
+
+const Oldhome = () => {
+  return  <Navigate to="/"  />;
+};
+
+export default Oldhome;
